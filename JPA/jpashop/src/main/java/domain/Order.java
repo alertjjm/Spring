72ec41d9d2906ae9619e58f9f1a4f4bff85d145e
alertjjm <sdr2936@gmail.com>
2021-03-14 21:43:32 +0900

    [JPA]

--- JPA/jpashop/src/main/java/domain/Order.java
+++ JPA/jpashop/src/main/java/domain/Order.java
@@ -12,16 +12,16 @@ public class Order extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ORDER_ID")
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
     private LocalDateTime orderDate;
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
     List<OrderItem> orderItems=new ArrayList<>();
     public Member getMember() {
         return member;
