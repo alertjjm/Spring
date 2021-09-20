75390eee3fc175ec7eb78374c3780bde4314c0f6
Jung Jongmin <sdr2936@gmail.com>
2021-09-20 16:49:51 +0900

    Feat: Spring Batch Chapter4

--- batch/src/main/java/com/example/batch/job/SimpleJobConfiguration.java
+++ batch/src/main/java/com/example/batch/job/SimpleJobConfiguration.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,17 +24,19 @@ public class SimpleJobConfiguration {
     @Bean
     public Job simpleJob(){
         return jobBuilderFactory.get("simpleJob") //simpleJob이란 임의의 이름의 Batch job 생성
-                .start(simpleStep1()) //simpleStep1 이란 이름의 Batch Step을 생성
+                .start(simpleStep1(null)) //simpleStep1 이란 이름의 Batch Step을 생성
                 .build();
     }
 
     @Bean
-    public Step simpleStep1(){
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate){
         return stepBuilderFactory.get("simpleStep1")
                 //Tasklet은 어찌보면 Spring MVC의 @Component, @Bean과 비슷한 역할이라고 보셔도 될 것 같습니다.
                 //명확한 역할은 없지만, 개발자가 지정한 커스텀한 기능을 위한 단위
                 .tasklet(((contribution, chunkContext) -> { //Tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할때 사용
                     log.info(">>> This is Step1");
+                    log.info(">>> requestDate = {}",requestDate);
                     return RepeatStatus.FINISHED;
                 }))
                 .build();
